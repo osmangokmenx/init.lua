9153cd6651bd60744a6078bd5076733031651afa
jengal-osman-gokmen <osman.gokmen@jengal.com>
2023-10-23 12:52:58 +0300

    alpha header

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,52 +1,62 @@
 return {
-    "goolord/alpha-nvim",
-    config = function()
-        local dashboard = require("alpha.themes.dashboard")
+  "goolord/alpha-nvim",
+  config = function()
+    local dashboard = require("alpha.themes.dashboard")
+    dashboard.section.header.val = [[
 
-        dashboard.section.header.val = {[[]],
-                                        [[                                                                              ]],
-                                        [[                                    ██████                                    ]],
-                                        [[                                ████▒▒▒▒▒▒████                                ]],
-                                        [[                              ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                              ]],
-                                        [[                            ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                            ]],
-                                        [[                          ██▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒                              ]],
-                                        [[                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▓▓▓▓                          ]],
-                                        [[                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▒▒▓▓                          ]],
-                                        [[                        ██▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒    ██                        ]],
-                                        [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
-                                        [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
-                                        [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
-                                        [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
-                                        [[                        ██▒▒██▒▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒██                        ]],
-                                        [[                        ████  ██▒▒██  ██▒▒▒▒██  ██▒▒██                        ]],
-                                        [[                        ██      ██      ████      ████                        ]],
-                                        [[                                                                              ]],
-                                        [[                                                                              ]],
-                                        [[]]}
+    ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ 
+    ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ 
+    ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ 
+    ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ 
+    ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ 
+    ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ 
 
-        dashboard.section.buttons.val =
-            {dashboard.button("d", " " .. " File browser", ":Telescope file_browser<CR>"),
-             dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-             dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-            -- dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
-             dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-             dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
-             dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
-             dashboard.button("q", " " .. " Quit", ":qa<CR>")}
+    ]]
 
-        local function footer()
-            return "Do Better"
-        end
+    -- dashboard.section.header.val = {[[]],
+    --                                 [[                                                                              ]],
+    --                                 [[                                    ██████                                    ]],
+    --                                 [[                                ████▒▒▒▒▒▒████                                ]],
+    --                                 [[                              ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                              ]],
+    --                                 [[                            ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                            ]],
+    --                                 [[                          ██▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒                              ]],
+    --                                 [[                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▓▓▓▓                          ]],
+    --                                 [[                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▒▒▓▓                          ]],
+    --                                 [[                        ██▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒    ██                        ]],
+    --                                 [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+    --                                 [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+    --                                 [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+    --                                 [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+    --                                 [[                        ██▒▒██▒▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒██                        ]],
+    --                                 [[                        ████  ██▒▒██  ██▒▒▒▒██  ██▒▒██                        ]],
+    --                                 [[                        ██      ██      ████      ████                        ]],
+    --                                 [[                                                                              ]],
+    --                                 [[                                                                              ]],
+    --                                 [[]]}
 
-        dashboard.section.footer.val = footer()
+    dashboard.section.buttons.val =
+    {dashboard.button("d", " " .. " File browser", ":Telescope file_browser<CR>"),
+    dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+    dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+    -- dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+    dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+    dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
+    dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+    dashboard.button("q", " " .. " Quit", ":qa<CR>")}
 
-        dashboard.section.footer.opts.hl = "Type"
-        dashboard.section.header.opts.hl = "Comment"
-        dashboard.section.buttons.opts.hl = "Keyword"
-
-        dashboard.opts.opts.noautocmd = true
-        require("alpha").setup(dashboard.opts)
-
+    local function footer()
+      return "Do Better"
     end
+
+    dashboard.section.footer.val = footer()
+
+    dashboard.section.footer.opts.hl = "Type"
+    dashboard.section.header.opts.hl = "Comment"
+    dashboard.section.buttons.opts.hl = "Keyword"
+
+    dashboard.opts.opts.noautocmd = true
+    require("alpha").setup(dashboard.opts)
+
+  end
 }
 
